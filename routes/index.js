e96d35258edfaf22faee3070bd8d5fd797220d3c
Yan-Rong-Chen <vicky1274@gmail.com>
2020-02-08 14:34:47 +0800

    change to ejs and add navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', username: '王大明' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.send("helloworld");
 });
 
 module.exports = router;
