4d200e4e68696c39747a35363efa02279a601074
Yan-Rong-Chen <vicky1274@gmail.com>
2020-02-17 19:19:06 +0800

    connect db with RESTful API

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -22,10 +22,10 @@ const getUserById = (request, response) => {
     const id = parseInt(request.params.id)
 
     pool.query('SELECT * FROM public."Project" WHERE pro_id = $1', [id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
+      if (error) {
+      throw error
+      }
+      response.status(200).json(results.rows)
     })
 }
 
