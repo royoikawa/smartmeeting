fa1272d94aba5a567d1840bd4067b08bdaf679f8
曾美佳 <y0943609436@gmail.com>
2020-02-18 19:57:00 +0800

    change btn color

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 var pg = require("pg")
 var connectionString = "pg://root:12345@localhost:5432/smartmeeting"
+//pg://你自己的用戶名:你自己的密碼@域名(沒有server所以用localhost):port號(預設5432)/你自己的database name
 var client = new pg.Client(connectionString)
 client.connect();
 
