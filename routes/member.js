39c96924475cefef7cde9ef99bd54e2c9b79e7c2
Yan-Rong-Chen <vicky1274@gmail.com>
2020-03-19 20:33:16 +0800

    member minutes UI

--- routes/member.js
+++ routes/member.js
@@ -4,7 +4,12 @@ var pool = require('../models/db');
 
 router.get('/:id_join', function(req, res, next) {
     var pro_id = req.params.id_join;
-    res.render('member', { title: 'SmartMeeting', username: req.session.userName, pro_id: pro_id});
+    var q = 'SELECT pro_name FROM project WHERE pro_id = $1';
+    pool.query(q, [pro_id], function(err, results) {
+        if (err) throw err;
+        var pro_name = results.rows[0].pro_name;
+        res.render('member', { title: 'SmartMeeting', username: req.session.userName, pro_id: pro_id, pro_name: pro_name });
+    })
 });
 
 module.exports = router;
